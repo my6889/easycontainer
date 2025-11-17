05d5fffdd50c8a1f221f0c9d70fa9b76030a8941
unknown <sunshiwei@socmap.net>
2025-11-17 17:33:07 +0800

    优化MySQL备份脚本

--- mysql-docker/mysqldump.sh
+++ mysql-docker/mysqldump.sh
@@ -2,4 +2,4 @@
 set -e
 DATE=$(date +%Y%m%d-%H%M%S)
 docker exec mysql-server mkdir -p /data/mysqldump
-docker exec mysql-server bash -c "mysqldump -uroot -pYOURPASSWORD | gzip -c > /data/mysqldump/mysqldumpall-$DATE.sql.gz"
+docker exec mysql-server bash -c "mysqldump -uroot --password='YOURPASSWORD' --all-databases | gzip -c > /data/mysqldump/mysqldumpall-$DATE.sql.gz"
